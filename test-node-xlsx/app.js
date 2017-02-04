8fb5ad53b5de28c0a941e907e2519740b5d2a498
Cedric-song <kengmun@126.com>
2017-02-04 23:24:11 +0800

    使用sqlite存储导入的数据，生成新表

--- test-node-xlsx/app.js
+++ test-node-xlsx/app.js
@@ -3,8 +3,7 @@ const app = express();
 const xlsx = require("node-xlsx");
 
 
-const list = xlsx.parse(__dirname + '/test.xlsx');
-
+const list = xlsx.parse(__dirname + '/test.xlsx')[0].data;
 app.get('/list', function (req, res) {
   res.send({data:list})
 })
@@ -13,7 +12,27 @@ const server = app.listen(7777, function () {
 
   const host = server.address().address
   const port = server.address().port
-
+  
   console.log('Server is working!')
 })
 
+
+const file = __dirname + '/db/test-node-xlsx.db'
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(file);
+
+db.serialize(function() {
+
+  var stmt = db.prepare("INSERT INTO record VALUES (?,?,?,?,?)");
+  for (var i = 1; i < list.length-1; i++) {
+      stmt.run(list[i][1],list[i][3],list[i][5],list[i][9],list[i][10]);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, name FROM record", function(err, row) {
+      console.log(row.name);
+  });
+});
+
+db.close();
